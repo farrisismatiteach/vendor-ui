0f99f7ae708caecf0177f77cfae38d96a1f65859
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-03-27 22:54:46 -0400

    amplify role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
       principals {
         type = "Service"
-        identifiers = ["ampilfy.amazonaws.com"]
+        identifiers = ["amplify.amazonaws.com"]
       }
     }
 }
