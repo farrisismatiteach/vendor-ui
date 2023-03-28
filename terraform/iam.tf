c31ea0eb12bc30cd9eda82a3cdacbfe5b4a8bd28
farrisismatiteach <farris.ismati.teach@gmail.com>
2023-03-27 22:49:08 -0400

    amplify role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
 data "aws_iam_policy_document" "assume_role_policy" {
     statement {
-      action = ["sts:AssumeRole"]
+      actions = ["sts:AssumeRole"]
 
       principals {
         type = "Service"
